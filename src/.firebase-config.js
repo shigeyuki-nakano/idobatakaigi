ed2a083f4b2a82586dac857d9909ef4377c985ab
shigeyuki-nakano <51395472+shigeyuki-nakano@users.noreply.github.com>
2020-12-02 13:41:21 +0900

    hard corded env vars

--- src/.firebase-config.js
+++ src/.firebase-config.js
@@ -1,13 +1,23 @@
 import firebase from 'firebase';
 
+const {
+    REACT_APP_FIREBASE_API_KEY,
+    REACT_APP_FIREBASE_AUTH_DOMAIN,
+    REACT_APP_FIREBASE_DATABASE_URL,
+    REACT_APP_FIREBASE_PROJECT_ID,
+    REACT_APP_FIREBASE_STORAGE_BUCKET,
+    REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    REACT_APP_FIREBASE_APP_ID
+} = process.env
+
 const firebaseConfig = {
-    apiKey: "AIzaSyCCdXTC_AinbnjhKBmv_KixFY8qpSvYzqE",
-    authDomain: "idobatakaigi-test.firebaseapp.com",
-    databaseURL: "https://idobatakaigi-test.firebaseio.com",
-    projectId: "idobatakaigi-test",
-    storageBucket: "idobatakaigi-test.appspot.com",
-    messagingSenderId: "808118216006",
-    appId: "1:808118216006:web:db5c5421f22d9dd989b147"
+    apiKey: REACT_APP_FIREBASE_API_KEY,
+    authDomain: REACT_APP_FIREBASE_AUTH_DOMAIN,
+    databaseURL: REACT_APP_FIREBASE_DATABASE_URL,
+    projectId: REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: REACT_APP_FIREBASE_APP_ID
 };
 
 firebase.initializeApp(firebaseConfig);
