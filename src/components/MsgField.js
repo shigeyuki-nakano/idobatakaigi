ac16750cd7b845ea6bdefded1a64f9a7f3328623
shigeyuki-nakano <51395472+shigeyuki-nakano@users.noreply.github.com>
2020-12-02 16:21:00 +0900

    message scroll into view

--- src/components/MsgField.js
+++ src/components/MsgField.js
@@ -1,9 +1,17 @@
 import React from 'react';
 
-export const MsgField = (props) => {
-    const {name, msg} = props
+export const MsgField = ({name, msg, isLastItem}) => {
+    const ref = React.useRef(null);
+
+    React.useEffect(() => {
+        if(isLastItem) {
+            ref.current.scrollIntoView({
+                behavior: "smooth"
+            });
+        }
+    }, [isLastItem])
     return (
-        <div style={{border: "1px solid black", marginBottom: "5px", padding: "5px"}}>
+        <div ref={(div) => ref.current = div} style={{border: "1px solid black", marginBottom: "5px", padding: "5px"}}>
             <div style={{borderBottom: "1px solid black", display: "inline"}}>{name}</div>
             <div>{msg}</div>
         </div>
--- src/components/MsgFieldList.js
+++ src/components/MsgFieldList.js
@@ -4,31 +4,37 @@ import { messagesRef } from '../.firebase-config';
 
 export const MsgFieldList = () => {
     const [messages, setMessages] = React.useState([]);
+    const [messagesLength, setMessagesLength] = React.useState(0);
 
     React.useEffect(() => {
         messagesRef
         .orderByKey()
-        .limitToLast(5)
+        // .limitToLast(5)
         .on('value', (data) => {
             const messages = data.val();
-            if(messages){
-                const entries = Object.entries(messages);
-        
-                const newMessages = entries.map((entry) => {
-                    const [key, nameAndText] = entry
-                    return {key: key, ...nameAndText}
-                })
-                setMessages(newMessages);
-            }
+            if(messages === null) {return};
+
+            const entries = Object.entries(messages);
+    
+            const newMessages = entries.map((entry) => {
+                const [key, nameAndText] = entry
+                return {key: key, ...nameAndText}
+            })
+            setMessages(newMessages);
         })
     }, []);
+
+    React.useEffect(() => {
+        setMessagesLength(messages.length);
+    }, [messages])
     return (
         <div>
-            {messages.map((message) => (
+            {messages.map((message, i) => (
                 <MsgField
                     key={message.key}
                     name={message.name}
                     msg={message.text}
+                    isLastItem={messagesLength === i}
                 />
             ))}
         </div>
